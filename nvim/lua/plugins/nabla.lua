cab1725d932efa26befa8dbcc74b6cda2d59a4aa
JP Appel <jeanpierre.appel01@gmail.com>
2023-09-01 09:35:21 -0400

    added tex equation preview via nabla

---
+++ nvim/lua/plugins/nabla.lua
@@ -0,0 +1,19 @@
+return {
+    'jbyuki/nabla.nvim',
+    dependencies = {
+         'nvim-treesitter/nvim-treesitter'
+    },
+    ft = {'markdown', 'pandoc', 'rmd'},
+    keys = {
+        { "<Leader>=p", function()
+            require('nabla').popup()
+        end,
+        desc = "Popup Nabla equation preview"
+        },
+        { "<Leader>==", function()
+            require('nabla').toggle_virt()
+        end,
+        desc = "Toggle Nabla equation preview for the entire buffer"
+        }
+    }
+}
