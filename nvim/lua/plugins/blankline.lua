2ca0bea095e4f29a7a4673a09fec23c65041877a
Jean-Pierre Appel <jeanpierre.appel01@gmail.com>
2023-10-03 12:26:46 -0400

    updated blankline to v3

--- nvim/lua/plugins/blankline.lua
+++ nvim/lua/plugins/blankline.lua
@@ -1,10 +1,11 @@
 -- TODO: configure tree-sitter to color current indentation level
 return {
     'lukas-reineke/indent-blankline.nvim', -- show indentation levels
+    main = "ibl",
     dependencies = 'nvim-treesitter/nvim-treesitter',
-    opts = {
-            show_current_context = true,
-            show_current_context_start = true,
-            indent_blankline_use_treesiter = true,
-        }
+    -- opts = {
+    --         show_current_context = true,
+    --         show_current_context_start = true,
+    --         indent_blankline_use_treesiter = true,
+    --     }
 }
