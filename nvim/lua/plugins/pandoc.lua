f0f0c52c07b9ac7f9cf7e6fab2ccd4fcf6578b58
Jean-Pierre Appel <jeanpierre.appel01@gmail.com>
2024-01-19 18:37:04 -0500

    feat: toc bind markdown files

--- nvim/lua/plugins/pandoc.lua
+++ nvim/lua/plugins/pandoc.lua
@@ -1,7 +1,10 @@
 return {
     {
         'vim-pandoc/vim-pandoc',
-        ft = {"markdown", "pandoc"}
+        ft = {"markdown", "pandoc"},
+        keys = {
+            {'gO', vim.cmd.TOC, desc = "Markdown file Table of Contents"}
+        }
     },
     {
         'vim-pandoc/vim-pandoc-syntax',
