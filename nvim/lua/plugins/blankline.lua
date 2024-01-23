9a24a7092103e4e9a3c80a978ef5deb750ceb605
Jean-Pierre Appel <jeanpierre.appel01@gmail.com>
2024-01-23 10:12:39 -0500

    refactor: fix blankline config

--- nvim/lua/plugins/blankline.lua
+++ nvim/lua/plugins/blankline.lua
@@ -3,9 +3,7 @@ return {
     'lukas-reineke/indent-blankline.nvim', -- show indentation levels
     main = "ibl",
     dependencies = 'nvim-treesitter/nvim-treesitter',
-    -- opts = {
-    --         show_current_context = true,
-    --         show_current_context_start = true,
-    --         indent_blankline_use_treesiter = true,
-    --     }
+    opts = {
+        scope = { enabled = true }
+        }
 }
