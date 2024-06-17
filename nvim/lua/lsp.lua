9e056fe0e15ac6a7ccd0d4a1c1788b7fb21f11e1
JP Appel <jeanpierre.appel01@gmail.com>
2024-06-17 18:13:44 -0400

    add texlab ls config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,8 +1,9 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
+local lsp_config = require('lspconfig')
 
-lsp.preset({})
+lsp_zero.preset({})
 
-lsp.ensure_installed({
+lsp_zero.ensure_installed({
     'pylsp',
     'html',
     'clangd',
@@ -16,22 +17,23 @@ lsp.ensure_installed({
     --'rust_analyzer'
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-lsp.set_preferences({
+lsp_zero.set_preferences({
     sign_icons = { }
 })
 
 
 -- LS configs
 --
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp_config.lua_ls.setup(lsp_zero.nvim_lua_ls())
+lsp_config.texlab.setup({})
 
 -- TODO: change bind for lsp.buf.code_actions()
 
 
-lsp.setup()
+lsp_zero.setup()
