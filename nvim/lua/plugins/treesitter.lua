d3d3d729a71820fc2a74ef44c055bf8dbb7cf8e7
JP Appel <jeanpierre.appel01@gmail.com>
2024-06-18 14:27:49 -0400

    add vim to ts parser list

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-        ensure_installed = { "help", "lua", "python", "java", "javascript", "typescript", "c", "latex" },
+        ensure_installed = { "help", "lua", "python", "java", "javascript", "typescript", "c", "latex", "vim" },
         auto_install = true,
         highlight = {
             enable = true,
