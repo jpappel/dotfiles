38f6ac9103cbbb47d210d4209efdee2331791ca9
JP Appel <jeanpierre.appel01@gmail.com>
2024-12-23 15:43:35 -0500

    Change some telescope themes

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,11 +3,11 @@
 -- ====================
 
 HOME = os.getenv("HOME")
-HOSTNAME = vim.loop.os_gethostname()
+HOSTNAME = vim.uv.os_gethostname()
 
 -- bootstrap lazynvim if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,45 +1,74 @@
 return {
-    'nvim-telescope/telescope.nvim',  -- fuzzy finder
+    'nvim-telescope/telescope.nvim', -- fuzzy finder
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons'
     },
     cmd = 'Telescope',
     keys = {
-        {"<leader>ff", function()
-            require("telescope.builtin").find_files()
-        end,
-        desc = "Search for files (respcting .gitignore)"
-    },
-    {"<leader>fb", function()
-        require("telescope.builtin").buffers()
-    end
-    },
-    {"<leader>fg", function()
-        require("telescope.builtin").live_grep()
-    end
-    },
-    {"<leader>fq", function()
-        require("telescope.builtin").quickfix()
-    end
-    },
-    {"<leader>fl", function()
-        require("telescope.builtin").loclist()
-    end
-    },
-    {"<leader>fd", function()
-        require("telescope.builtin").diagnostics()
-    end
-    },
-    {"<leader>fr", function()
-        -- TODO: if lsp supports reference provider and lsp is attached
-        -- [telescope.builtin.lsp_*]: server does not support referencesProvider
-        if #vim.lsp.get_clients({bufnr = 0}) > 0 then
-            require("telescope.builtin").lsp_references()
-        else
-            require("telescope.builtin").grep_string()
+        {
+            "<leader>ff",
+            function()
+                local builtin = require("telescope.builtin")
+                builtin.find_files()
+            end,
+            desc = "Search for files (respcting .gitignore) using Telescope"
+        },
+        {
+            "<leader>fF",
+            function()
+                require("telescope.builtin").find_files({ hidden = true, no_ignore_parent = true })
+            end,
+            desc = "Search for files (including hidden) using Telescope"
+        },
+        {
+            "<leader>fb",
+            function()
+                require("telescope.builtin").buffers()
+            end,
+            desc = "Search for open buffers using Telescope"
+        },
+        { "<leader>fg", function()
+            require("telescope.builtin").live_grep()
         end
-    end
-    }
+        },
+        {
+            "<leader>fq",
+            function()
+                local themes = require("telescope.themes")
+                require("telescope.builtin").quickfix(themes.get_ivy({}))
+            end,
+            desc = "Search in the the quickfix buffer using Telescope"
+        },
+        {
+            "<leader>fl",
+            function()
+                local themes = require("telescope.themes")
+                require("telescope.builtin").loclist(themes.get_ivy({}))
+            end,
+            desc = "Search within a location list using Telescope"
+        },
+        {
+            "<leader>fd",
+            function()
+                local themes = require("telescope.themes")
+                require("telescope.builtin").diagnostics(themes.get_ivy({}))
+            end,
+            desc = "Search through diagnostics using Telescope"
+        },
+        {
+            "<leader>fr",
+            function()
+                -- TODO: if lsp supports reference provider and lsp is attached
+                -- [telescope.builtin.lsp_*]: server does not support referencesProvider
+                local themes = require("telescope.themes")
+                if #vim.lsp.get_clients({ bufnr = 0 }) > 0 then
+                    require("telescope.builtin").lsp_references(themes.get_ivy({}))
+                else
+                    require("telescope.builtin").grep_string(themes.get_ivy({}))
+                end
+            end,
+            desc = "Search for refences via grep or a language server using Telescope"
+        }
     }
 }
