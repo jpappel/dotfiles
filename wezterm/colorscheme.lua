3d94debbc557bcc0782e1a5bb5aa4db5d5426120
JP Appel <jeanpierre.appel01@gmail.com>
2025-06-14 14:17:52 -0400

    Update wezterm config

--- wezterm/colorscheme.lua
+++ wezterm/colorscheme.lua
@@ -1,70 +1,81 @@
 local M = {}
 
+
+local WalTheme = {
+    Colorscheme = {
+        background = '#011111',
+        foreground = '#cfc797',
+
+        cursor_bg = '#732d35',
+        cursor_fg = '#e4a44d',
+        compose_cursor = '#e4a44d',
+
+        cursor_border = '#732d35',
+
+        selection_bg = '#003136',
+        ansi = {
+            '#091919', -- black
+            '#732d35', -- red
+            '#438566', -- green
+            '#ab710e', -- yellow
+            '#254e76', -- blue
+            '#643875', -- purple
+            '#136783', -- cyan
+            '#bfc3c3', -- white
+        },
+        brights = {
+            '#556767',
+            '#945d63',
+            '#7ba47d',
+            '#e4a44d',
+            '#49698f',
+            '#815e8a',
+            '#498ba1',
+            '#e6e7e7',
+        },
+
+        visual_bell = '#732d35',
+
+        tab_bar = {
+            background = '#011111',
+
+            active_tab = {
+                bg_color = '#438566',
+                fg_color = '#011111',
+            },
+
+            inactive_tab = {
+                bg_color = '#003136',
+                fg_color = '#cfc797',
+            },
+            inactive_tab_hover = {
+                bg_color = '#011111',
+                fg_color = '#bfc3c3',
+            },
+
+            new_tab = {
+                bg_color = '#136783',
+                fg_color = '#cfc797',
+            },
+            new_tab_hover = {
+                bg_color = '#011111',
+                fg_color = '#bfc3c3',
+            },
+        },
+    },
+    apply_to_config = function(config)
+        config.command_palette_fg_color = '#cfc797'
+        config.command_palette_bg_color = '#438566'
+    end
+}
+
 function M.apply_to_config(config)
     config.color_scheme = 'Wal'
     -- color schemes
     config.color_schemes = {
-        ['Wal'] = {
-            background = '#011111',
-            foreground = '#cfc797',
-
-            cursor_bg = '#732d35',
-            cursor_fg = '#e4a44d',
-
-            cursor_border = '#732d35',
-
-            selection_bg = '#003136',
-            ansi = {
-                '#091919', -- black
-                '#732d35', -- red
-                '#438566', -- green
-                '#ab710e', -- yellow
-                '#254e76', -- blue
-                '#643875', -- purple
-                '#136783', -- cyan
-                '#bfc3c3', -- white
-            },
-            brights = {
-                '#556767',
-                '#945d63',
-                '#7ba47d',
-                '#e4a44d',
-                '#49698f',
-                '#815e8a',
-                '#498ba1',
-                '#e6e7e7',
-            },
-
-            visual_bell = '#732d35',
-
-            tab_bar = {
-                background = '#011111',
-
-                active_tab = {
-                    bg_color = '#438566',
-                    fg_color = '#011111',
-                },
-
-                inactive_tab = {
-                    bg_color = '#003136',
-                    fg_color = '#cfc797',
-                },
-                inactive_tab_hover = {
-                    bg_color = '#011111',
-                    fg_color = '#bfc3c3',
-                },
-
-                new_tab = {
-                    bg_color = '#136783',
-                    fg_color = '#cfc797',
-                },
-                new_tab_hover = {
-                    bg_color = '#011111',
-                    fg_color = '#bfc3c3',
-                },
-            },
-        }
+        ['Wal'] = WalTheme.Colorscheme
     }
+    WalTheme.apply_to_config(config)
 end
 
 return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,22 +2,24 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+require('keybinds').apply_to_config(config)
+
+-- tab
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- window appearnce
+config.font_size = 12
 config.window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
 }
-
-config.font_size = 12
-
--- tab
-config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-
--- window scheme/appeance
-require('colorscheme').apply_to_config(config)
 config.window_background_opacity = 0.75
+
+-- colors
+require('colorscheme').apply_to_config(config)
 
 -- bell settings
 config.visual_bell = {
@@ -27,14 +29,9 @@ config.visual_bell = {
     fade_out_duration_ms = 150,
 }
 
-wezterm.on('bell', function(_, pane)
+wezterm.on('bell', function(window, pane)
     if pane:has_unseen_output() then
-        os.execute(table.concat({
-            "notify-send", "-a wezterm",
-            '"WezTerm: Bell Rung"',
-            '"' .. pane:get_title() .. '"',
-        }, " ")
-        )
+        window:toast_notification("WezTerm: Bell Rung", pane:get_title())
     end
 end)
 
