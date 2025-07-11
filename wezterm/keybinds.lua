af4fd3b1df0e1bfade496998dc7444cbeb1ae6a1
JP Appel <jeanpierre.appel01@gmail.com>
2025-07-11 12:53:06 -0400

    Update wezterm binds

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -2,42 +2,48 @@ local M = {}
 
 local wezterm = require 'wezterm'
 
-
-local disabled_keys = {
-    {
-        key = 'M',
-        mods = 'CTRL',
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = 'Enter',
-        mods = 'ALT',
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = 'P',
-        mods = 'CTRL',
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-}
-
 function M.apply_to_config(config)
     config.leader = {
         key = ';',
         mods = 'CTRL',
     }
-    config.keys = {}
-
-    for _, disabled_map in ipairs(disabled_keys) do
-        table.insert(config.keys, disabled_map)
-    end
-
-    table.insert(config.keys, {
-        key = ';',
-        mods = 'LEADER',
-        action = wezterm.action.ActivateCommandPalette,
-    })
-
+    config.keys = {
+        {
+            key = 'M',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment,
+        },
+        {
+            key = 'Enter',
+            mods = 'ALT',
+            action = wezterm.action.DisableDefaultAssignment,
+        },
+        {
+            key = 'P',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment,
+        },
+        {
+            key = 'X',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment,
+        },
+        {
+            key = ';',
+            mods = 'LEADER',
+            action = wezterm.action.ActivateCommandPalette,
+        },
+        {
+            key = '[',
+            mods = 'LEADER',
+            action = wezterm.action.ActivateCopyMode,
+        },
+        {
+            key = '/',
+            mods = 'LEADER',
+            action = wezterm.action.Search({ CaseInSensitiveString = ''})
+        },
+    }
 end
 
 return M
